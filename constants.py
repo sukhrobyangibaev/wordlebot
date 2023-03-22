642c41870f417fc091431f42cc3973f72a90b8d6
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2023-03-22 11:27:26 +0500

    add logging to file

--- constants.py
+++ constants.py
@@ -58,7 +58,9 @@ DEFAULT_ATTEMPTS = 6
 DEVELOPER_CHAT_ID = '1476403327'
 
 logging.basicConfig(
-    format="[%(asctime)s > %(levelname)s > %(name)s] %(message)s", level=logging.INFO
+    filename='dota_data_bot.log',
+    format="[%(asctime)s > %(levelname)s > %(name)s] %(message)s",
+    level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
